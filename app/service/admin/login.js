26abd613a2ac5829c564dbb75c3e293344c117e3
wangweijie0418@163.com <wangweijie0418@163.com>
2021-08-21 14:13:42 +0800

    修改

--- app/service/admin/login.js
+++ app/service/admin/login.js
@@ -4,7 +4,6 @@ const Service = require("egg").Service;
 
 class LoginService extends Service {
   async findUser() {
-    console.log(this.ctx.request);
     const username = this.ctx.request.body.username;
     const password = this.ctx.request.body.password;
     try {
